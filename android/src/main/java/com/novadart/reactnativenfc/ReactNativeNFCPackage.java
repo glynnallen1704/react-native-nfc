fdc4162f3bc086fa7ac320f208b448366a6eb396
Glynn Allen <glynn.allen@me.com>
2019-12-24 09:55:41 +0200

    Update ReactNativeNFCPackage.java
    Removed Overrides

--- android/src/main/java/com/novadart/reactnativenfc/ReactNativeNFCPackage.java
+++ android/src/main/java/com/novadart/reactnativenfc/ReactNativeNFCPackage.java
@@ -11,19 +11,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class ReactNativeNFCPackage implements ReactPackage {
-    @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>(1);
         modules.add(new ReactNativeNFCModule(reactContext));
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
